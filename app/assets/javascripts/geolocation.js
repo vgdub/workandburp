b8ec9b5422dc20676f8df067bdc18943510f1048
vgdub <vincentgwilliams@gmail.com>
2012-07-28 13:24:33 -0500

    adding views

---
+++ app/assets/javascripts/geolocation.js
@@ -0,0 +1,19 @@
+// if (navigator.geolocation){
+// 		navigator.geolocation.getCurrentPosition(
+//                                     success_handler, 
+//                                     error_handler, 
+//                                     {enableHighAccuracy:true});
+// 	}
+// 	else {
+// 		alert('Geolocation not available')
+// 	}
+// 	function success_handler(position) {
+//         latitude = position.coords.latitude;
+//         longitude = position.coords.longitude;
+//         accuracy = position.coords.accuracy;
+
+//         $.cookie("posLat", latitude);
+//         $.cookie("posLon", longitude);
+//         $.cookie("posAccuracy", accuracy);
+//     }
+
