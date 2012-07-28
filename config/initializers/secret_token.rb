34af380f785e8f1fd4480e24a923ecd62936d962
vgdub <vincentgwilliams@gmail.com>
2012-07-27 20:32:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkAndBurp::Application.config.secret_token = '75db6ad26bdebc7e5d93adf853b93b1f0f670d2e6a98d3a1190a8b80afbac26ff1b07379d130b5a894e73ae7d932c3d778d7953d0526204ed8d2180544093eb6'
