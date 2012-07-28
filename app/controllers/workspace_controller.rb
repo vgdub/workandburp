9e1d1aa114ddf17e6e19c410a1b1a20aa82d19fb
Claire Feinberg <c.a.feinberg@gmail.com>
2012-07-28 00:33:36 -0700

    added stuff

---
+++ app/controllers/workspace_controller.rb
@@ -0,0 +1,28 @@
+class WorkspaceController < ApplicationController
+  def index
+    consumer_key = 'y6kx_rHeXbGuTAzI6Y1ouw'
+    consumer_secret = 'L85XGxL7KnneXEry_BGgDByb584'
+    token = 'uC4XhkX-jcecnAnw4mQAmVxxKsrocQDY'
+    token_secret = 'myoGPfJlOz3AIk1Tlj8eyq2L6Wo'
+
+    api_host = 'api.yelp.com'
+
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+    access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+
+    path = "/v2/search?term=restaurants&location=new%20york"
+
+    @response = JSON.parse(access_token.get(path).body)
+
+    # perform an address/location-based search for cream puffs nearby
+    # request = Yelp::V1::Review::Request::Location.new(
+    #             :address => '222 Merchandise Mart',
+    #             :city => 'Chicago',
+    #             :state => 'IL',
+    #             :radius => 2,
+    #             :term => 'pickles',
+    #             :yws_id => '8_OYsIQfLVSZtQpDx-cuVg')
+    # 
+    # @response = client.search(request)
+  end
+end
