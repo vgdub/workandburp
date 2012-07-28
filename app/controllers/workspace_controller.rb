f14d371699db424755a1673c0cf8dfe759a05c0b
vgdub <vincentgwilliams@gmail.com>
2012-07-28 15:19:27 -0500

    changes

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -14,12 +14,12 @@ class WorkspaceController < ApplicationController
 
     @search_term = "wifi"
     
-    # @local_zip = "60614"
+    @local_zip = "60654"
 
     consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
     access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-    path = "/v2/search?term=#{@search_term}&ll=#{@lat},#{@lon}&limit=5&radius_filter=2000&sort=1"
+    path = "/v2/search?term=#{@search_term}&location=#{@local_zip}&limit=5&radius_filter=2000&sort=1"
 
     @responseyelp = JSON.parse(access_token.get(path).body)
 
@@ -31,14 +31,17 @@ class WorkspaceController < ApplicationController
         return b
     end
 
-    @lat = split(latitude.to_f)
-    @lon = split(longitude.to_f)
+    @lat_string = split(latitude.to_f).to_s
+    @lon_string = split(longitude.to_f).to_s
 
-    # @lat = cookies[:lat].@split
-    # @lon = cookies[:lon].@split
+    @lat = @lat_string
+    @lon = @lon_string
+
+    # @lat = cookies[:lat].to_f
+    # @lon = cookies[:lon].to_f
     # @acc = cookies[:acc]
 
-    @response_citygrid = JSON.parse(open("http://api.citygridmedia.com/content/places/v2/search/latlon?what=B.Y.O.B&lat=#{@lat}&lon=#{@lon}&radius=7&page=1&rpp=3&sort=topmatches&publisher=test&format=json").read)
+    @response_citygrid = JSON.parse(open("http://api.citygridmedia.com/content/places/v2/search/latlon?what=B.Y.O.B&lat=#{@lat}&lon=#{@lon}&radius=7&page=1&rpp=5&sort=topmatches&publisher=test&format=json").read)
 
     
 
