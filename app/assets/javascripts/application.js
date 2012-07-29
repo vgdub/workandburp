ac74b563642fdd6a6f95e4b2ac48e79392d12bdd
vgdub <vincentgwilliams@gmail.com>
2012-07-29 14:11:18 -0500

    fixing geo for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,19 +30,7 @@
 //     document.cookie = "acc=" + escape(accuracy);
 // }
 
-function getGeoLocation() {
-  navigator.geolocation.getCurrentPosition(setGeoCookie);
-}
 
-function setGeoCookie(position) {
-	var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var accuracy = position.coords.accuracy;
-
-    document.cookie = "lat=" + escape(latitude);
-    document.cookie = "lon=" + escape(longitude);
-    document.cookie = "acc=" + escape(accuracy);
-}
 
 // var mapOption = {
 //         center = new google.maps.LatLng(41.888, 87.635),
